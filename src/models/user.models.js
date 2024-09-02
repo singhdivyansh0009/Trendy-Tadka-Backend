77d51cf240f9b1747869f1928ad394becb4559f0
singhdivyansh0009 <=>
2024-09-03 00:35:38 +0530

    added otp field in user

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -38,7 +38,8 @@ const userSchema = new Schema(
                 type : Schema.Types.ObjectId,
                 ref : "Videos"
             }
-        ]
+        ],
+        otp : String
     },{  timestamps: true }
 );
 
