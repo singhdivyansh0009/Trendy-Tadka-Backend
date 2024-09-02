9bb98810932fd7cdeba455597e7a02cd76290b83
singhdivyansh0009 <=>
2024-09-02 16:01:33 +0530

    added middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import express from "express";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(cookieParser());
+import userRouter from "./routes/register.routes.js";
+app.use('/api/v1/users', userRouter);  // middleware for user releated routes
 
 export default app;
