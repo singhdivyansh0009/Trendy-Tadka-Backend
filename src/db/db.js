4e6b8c248669949d37b52e38df8ab91701676681
unknown <singh divyansh>
2024-09-08 01:22:21 +0530

    Some more functionality added

--- src/db/db.js
+++ src/db/db.js
@@ -4,6 +4,7 @@ import { DB_NAME } from "../constants.js";
 // function to connect the database
 const connectDB = async () => {
        try{
+           console.log(`${process.env.MONGODB_URI}/${DB_NAME}`);
            const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
            console.log("Database Connected !! HOST:", connection.connections[0].host);
        }catch(err){
