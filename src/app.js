39c9821b72e0ac495c98313d0426783d89287da6
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2024-09-26 22:11:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,34 +2,38 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
+
+// Corrected CORS settings
 const corsOptions = {
-   origin: 'https://youtube-backend-1-sor8.onrender.com', // Replace with your frontend's domain
+   origin: 'https://trendy-tadka-frontend.vercel.app', // Frontend's domain
    credentials: true // Allow cookies to be sent and received
- };
+};
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(cookieParser());
-// base url
-app.get('/', (req,res) =>{
-     res.json({
-        message : "sever deployed sucessfully"
-     })
-})
-import userRouter from "./routes/user.routes.js";
-import vedioRouter from "./routes/video.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.route.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
-app.use('/api/v1/users', userRouter);  // middleware for user related routes
-app.use('/api/v1/video',vedioRouter); // middleware for vedio related routes
-app.use('/api/v1/subscription',subscriptionRouter); // middleware for subscription routes
-app.use('/api/v1/playlist',playlistRouter); // middleware for playlist routes
-app.use('/api/v1/comment',commentRouter); // middleware for comment routes
-app.use('/api/v1/like',likeRouter); // middleware for Like routes
-app.use("/api/v1/dashboard",dashboardRouter); // middleware for dashboard
 
-export default app;
+// base url
+app.get('/', (req, res) => {
+     res.json({
+        message: "server deployed successfully"
+     });
+});
+
+// Importing routes
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.route.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+
+// Using routes as middleware
+app.use('/api/v1/users', userRouter);  // middleware for user related routes
+app.use('/api/v1/video', videoRouter); // middleware for video related routes
+app.use('/api/v1/subscription', subscriptionRouter); // middleware for subscription routes
+app.use('/api/v1/playlist', playlistRouter); // middleware for playlist routes
+app.use('/api/v1/comment', commentRouter); // middleware for comment routes
+app.use('/api/v1/like', likeRouter); // middleware for Like routes
